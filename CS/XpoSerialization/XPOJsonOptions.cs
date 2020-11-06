8059e4e6d0e074537e555e9e328d92c969e8084a
Georgiy Trenin <georgiy.trenin@devexpress.com>
2020-11-06 13:32:07 +0300

    options

--- CS/XpoSerialization/XPOJsonOptions.cs
+++ CS/XpoSerialization/XPOJsonOptions.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
-
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using DevExpress.Xpo;
+using System.Linq;
 namespace XpoSerialization {
 
     internal class ConfigureJsonOptions : IConfigureOptions<JsonOptions>, IServiceProvider {
@@ -26,6 +28,36 @@ namespace XpoSerialization {
         }
     }
 
+    public class XpoMetadataProvider : DefaultModelMetadataProvider {
+        /// <summary>
+        /// Creates a new <see cref="XpoMetadataProvider"/>.
+        /// </summary>
+        /// <param name="detailsProvider">The <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.Metadata.ICompositeMetadataDetailsProvider"/>.</param>
+        public XpoMetadataProvider(ICompositeMetadataDetailsProvider detailsProvider) : base(detailsProvider) {
 
+        }
 
+        /// <summary>
+        /// Creates a new <see cref="XpoMetadataProvider"/>.
+        /// </summary>
+        /// <param name="detailsProvider">The <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.Metadata.ICompositeMetadataDetailsProvider"/>.</param>
+        /// <param name="optionsAccessor">The accessor for <see cref="Microsoft.AspNetCore.Mvc.MvcOptions"/>.</param>
+        public XpoMetadataProvider(ICompositeMetadataDetailsProvider detailsProvider, IOptions<MvcOptions> optionsAccessor) : base(detailsProvider, optionsAccessor) {
+
+        }
+
+        protected override DefaultMetadataDetails[] CreatePropertyDetails(ModelMetadataIdentity key) {
+            DefaultMetadataDetails[] result = base.CreatePropertyDetails(key);
+            if(typeof(PersistentBase).IsAssignableFrom(key.ModelType))
+                return result.Where(x => !IsServiceField(x.Key)).ToArray();
+            else
+                return result;
+        }
+
+        static bool IsServiceField(ModelMetadataIdentity identity) {
+            Type declaringType = identity.PropertyInfo.DeclaringType;
+            return declaringType == typeof(PersistentBase)
+                || declaringType == typeof(XPBaseObject);
+        }
+    }
 }
